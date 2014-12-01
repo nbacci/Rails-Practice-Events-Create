8686f0466dd84140f52a00bf5dbf51964a48f03f
nbacci <nmbachvarova@ucdavis.edu>
2014-12-01 14:28:35 -0800

    creates form_for, migration, adds an html table, and iterates over the events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to(events_path())
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(
+    :date,
+    :description,
+    :requires_id,
+    :capacity,
+    :directions,
+    )
+  end
+end
